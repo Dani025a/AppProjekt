d95ab66ae25cfa1e4c3330b12bb0ec174b4289ef
Danijel Gitanovic <dani025a@edu.ucl.dk>
2021-02-05 02:58:08 +0100

    Milepael 2 Tilføjet EditText, TextView og Button.

--- app/src/main/java/com/example/appprojekt/MainActivity.java
+++ app/src/main/java/com/example/appprojekt/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.appprojekt;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnAdd;
+        final TextView textDisplay;
+        final EditText textInput;
+
+        btnAdd = findViewById(R.id.btnAdd);
+        textDisplay = findViewById(R.id.textDisplay);
+        textInput = findViewById(R.id.textinput);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textDisplay.setText(textInput.getText());
+            }
+        });
     }
 }
